9844cf5a1767222009723c99c9ad8e10a871533f
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-08-11 23:45:51 -0300

    including update on EventService and EventController

--- src/main/java/com/saadeh/TDD_Event_City/controllers/EventController.java
+++ src/main/java/com/saadeh/TDD_Event_City/controllers/EventController.java
@@ -1,9 +1,29 @@
 package com.saadeh.TDD_Event_City.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saadeh.TDD_Event_City.dto.EventDTO;
+import com.saadeh.TDD_Event_City.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/events")
 public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto){
+        dto = service.update(id,dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.ok().body(dto);
+    }
 }
--- src/main/java/com/saadeh/TDD_Event_City/services/EventService.java
+++ src/main/java/com/saadeh/TDD_Event_City/services/EventService.java
@@ -1,4 +1,40 @@
 package com.saadeh.TDD_Event_City.services;
 
+import com.saadeh.TDD_Event_City.dto.EventDTO;
+import com.saadeh.TDD_Event_City.entities.Event;
+import com.saadeh.TDD_Event_City.repositories.CityRepository;
+import com.saadeh.TDD_Event_City.repositories.EventRepository;
+import com.saadeh.TDD_Event_City.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto){
+        try {
+            Event entity = repository.getReferenceById(id);
+            copyDtoToEntity(entity, dto);
+            repository.save(entity);
+            return new EventDTO(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    private void copyDtoToEntity(Event entity, EventDTO dto) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(cityRepository.getReferenceById(dto.getCityId()));
+    }
 }
