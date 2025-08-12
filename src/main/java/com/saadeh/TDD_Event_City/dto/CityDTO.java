f3ecd173d38861fd7138b343d9d03e6940fb962b
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-08-11 22:02:43 -0300

    Creating EventDTO and CityDTO

---
+++ src/main/java/com/saadeh/TDD_Event_City/dto/CityDTO.java
@@ -0,0 +1,41 @@
+package com.saadeh.TDD_Event_City.dto;
+
+import com.saadeh.TDD_Event_City.entities.City;
+
+import java.io.Serializable;
+
+public class CityDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public CityDTO() {
+    }
+
+    public CityDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CityDTO(City entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
