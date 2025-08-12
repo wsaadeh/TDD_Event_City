1ab5ddb9f12039c81d8d208c6e8a973f1f27da3e
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-08-11 21:54:16 -0300

    Project created

---
+++ src/main/java/com/saadeh/TDD_Event_City/TddEventCityApplication.java
@@ -0,0 +1,13 @@
+package com.saadeh.TDD_Event_City;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddEventCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddEventCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadeh/TDD_Event_City/TddEventCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadeh.TDD_Event_City;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddEventCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
